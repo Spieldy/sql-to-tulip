d985ba3f2c91fca5e9fec973aa441a5e63bad174
plfaure <plouis.faure@gmail.com>
2017-06-09 21:20:03 -0400

    Attribute class for attributes on graph nodes

---
+++ Attribute.py
@@ -0,0 +1,19 @@
+class Attribute:
+
+    def __init__(self, name, var_type, is_pk, is_fk):
+        self.name = name
+        self.type = var_type
+        self.is_pk = is_pk
+        self.is_fk = is_fk
+
+    def get_name(self):
+        return self.name
+
+    def get_type(self):
+        return self.type
+
+    def is_pk(self):
+        return self.is_pk
+
+    def is_fk(self):
+        return self.is_fk
