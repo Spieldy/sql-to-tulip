a67f34bb82a9160bdb801a261422e0255957a3cc
Theo <theo_menard@hotmail.fr>
2017-06-22 16:38:40 -0400

    Removed bugged part

--- GraphView.py
+++ GraphView.py
@@ -1,29 +1,37 @@
 from tulip import *
 from TableView import *
 
+
 class GraphView(object):
-  
-  def __init__(self, graph):
-    self.graph = graph
-    
-  def set_view(self):
-    self.compute_sizes()
-    self.set_edges()
-    self.graph.applyLayoutAlgorithm('Planarization Layout (OGDF)')
-    self.set_tables()
-    
-  def compute_sizes(self):
-    for n in self.graph.getNodes():
-      t = TableView(n, self.graph)
-      t.compute_size()
- 
-  def set_edges(self):
-    for e in self.graph.getEdges():
-      self.graph['viewSize'][e] = tlp.Size(1, 1, 1)
-      #self.graph['viewColor'][e] = tlp.Color(243, 134, 48)
-      
-  def set_tables(self):
-    for n in self.graph.getNodes():
-        t = TableView(n, self.graph)
-        t.compute_size()
-        t.display()
+    def __init__(self, graph):
+        self.graph = graph
+
+    def set_view(self):
+        self.compute_sizes()
+        self.set_edges()
+        self.graph.applyLayoutAlgorithm('Planarization Layout (OGDF)')
+        self.set_tables()
+        #self.set_properties()
+
+    def compute_sizes(self):
+        for n in self.graph.getNodes():
+            t = TableView(n, self.graph)
+            t.compute_size()
+
+    def set_edges(self):
+        for e in self.graph.getEdges():
+            self.graph['viewSize'][e] = tlp.Size(1, 1, 1)
+
+    def set_tables(self):
+        for n in self.graph.getNodes():
+            t = TableView(n, self.graph)
+            t.compute_size()
+            t.display()
+
+    def set_properties(self):
+        #should work, but it does not...
+        for v in tlpgui.getOpenedViews():
+            if isinstance(v, tlpgui.NodeLinkDiagramComponent):
+                params = v.getRenderingParameters()
+                params.setLabelFixedFontSize(True)
+                params.setEdgeSizeInterpolate(False)
